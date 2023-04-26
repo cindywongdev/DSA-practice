dc5ba93cc7ba25a3ffab9dc8f309947a66279322
Cindy Wong <cindywongdev@gmail.com>
2023-04-26 17:26:45 -0400

    step 1 done

---
+++ recursion/first-recursive-function.js
@@ -0,0 +1,7 @@
+// step 1: function and parameters
+// parameters are important!
+// give index and sum default values so that they don't need to be provided on the first call of the function
+
+function sumArrayOfNums(arr, index = 0, sum = 0){
+
+}
